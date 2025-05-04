831931db49640fdfa2ac1b0c72f3b58bd2195dc7
vBrambilla <nyari.gabor1986@gmail.com>
2025-05-04 17:16:23 +0200

    Add files via upload

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=c:\Users\nyari.gabor\Documents\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\nyari.gabor\Documents\RETORKI Feladatkezelo\RETORKI_2025_04_28\retorki_feladatkezelo_02"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
